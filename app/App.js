bb332fe5b633c59455358cc3ab4cc15541008457
Hummara Bashir <bashirh@state.gov>
2023-09-27 15:43:00 -0400

    phase

--- app/App.js
+++ app/App.js
@@ -11,6 +11,7 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import {createRootNavigator} from './router';
 import Home from './screens/Home';
 import Buttons from './screens/Buttons';
+import Phase2 from './screens/Phase2';
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
@@ -24,6 +25,8 @@ export default function App() {
         <Stack.Navigator> 
           <Stack.Screen name="Home" component = {Home} /> 
           <Stack.Screen name="Buttons" component = {Buttons} /> 
+          <Stack.Screen name="Phase2" component = {Phase2} /> 
+
         </Stack.Navigator> 
       </NavigationContainer> 
     );
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -17,7 +17,9 @@ import {
   Pressable,
   Image,
   Alert,
-  TouchableOpacity
+  TouchableOpacity,
+  Button,
+  ImageBackground
 } from 'react-native';
 
 import {
@@ -31,25 +33,16 @@ import { SvgXml } from 'react-native-svg';
 import Greetings from './screens/greetings';
 import { createStackNavigator } from '@react-navigation/native-stack';
 
-// import Blob from './assets/images/blob.svg';
-// source={ { uri: "https://facebook.github.io/react-native/img/header_logo.png" } }
-const xml = `
-<svg width="348" height="384" viewBox="0 0 348 384" fill="none">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M193.176 379.644C152.654 377.186 115.472 363.874 83.2145 339.226C49.2502 313.274 20.7413 281.238 10.1808 239.818C-2.44003 190.319 -6.55844 136.162 20.3722 92.754C50.5348 44.1369 101.577 -7.89928 158.005 1.55049C213.602 10.8612 221.223 87.0191 255.114 132.065C275.93 159.732 304.061 178.837 315.874 211.382C332.965 258.468 365.382 314.075 335.868 354.549C306.288 395.114 243.288 382.683 193.176 379.644Z" fill="#294461"/>
-</svg>
 
-`;
-export default function App() {
-  return (
-    <>
-     <StatusBar style="light" />
-     <SafeAreaView style={styles.container}>
-     <ScrollView style={styles.scrollView}>
+const Home = ({navigation}) => (
+  <>
+  <StatusBar style="light" />
+  <View style={styles.container}>
+    <ImageBackground source={require('../assets/images/blob.png')} resizeMode="cover" style={styles.bgimage}>
+      {/* <Text style={styles.text}>Inside</Text> */}
+      <View style={styles.contain}>
 
-    <View style={styles.container}>
-   <SvgXml xml={xml} style={styles.blob}/>
-    {/* <ImageBackground source={require('./assets/images/blob.svg')} resizeMode="cover" style={styles.bgimage}> */}
-   <View style={styles.row}>
+      <View style={styles.row}>
     <View style={styles.logocontainer}>
 <Image
                   source={require('../assets/images/bee-logo.png')}
@@ -61,61 +54,88 @@ export default function App() {
                 </View>
                 </View>
                 {/* </ImageBackground> */}
+
                 <Greetings/>
-                <TouchableOpacity onPress={() => this.navigation.navigate('Buttons')}>
+                {/* <TouchableOpacity onPress={() => this.props.navigation.navigate('Buttons')}>
                     <Text>Let's GO -></Text>
-</TouchableOpacity>
-               
-    </View>
-    </ScrollView>
-    </SafeAreaView>
-    </>
-  )};
-  var styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      height: '100%',
-      backgroundColor: '#1f354b',
-      fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif'
-    },
-    bgimage:{
-      flex: 1,
-      justifyContent: 'center',
-    },
-    blob: {
-      marginTop: -150
-    },
-    image: {
+</TouchableOpacity> */}
+<View style={styles.btn}>
+  <Button color="#ffffff"
+
+        title="Let's Go ->"
+        onPress={() => navigation.navigate('Buttons')}
+      />
+      </View>
+      </View>
+
+    </ImageBackground>
+  </View>
+  </>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    backgroundColor: '#1f354b',
+    fontFamily: '-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif'
+  },
+  bgimage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000c0',
+  },
+  image: {
     width: 80,
     height: 80,  
     margin: 5
   },
-title: {
-  color: '#f7bf31',
-  fontWeight: '900',
-  fontSize: 30,
-  textAlign: 'center',
-  alignItems: 'center',
-  justifyContent: 'center',
-
-},
-titlespan: {
-  color: '#ffffff',
-  fontWeight: '200'
-},
-row: {
-  flexDirection: 'row',
-  flexWrap: 'wrap',
-  marginTop: -200
-
-},
-logocontainer: {
-  flexDirection: 'column',
-  textAlign: 'center',
-  alignItems: 'center',
-  justifyContent: 'center',
-},
+  title: {
+    color: '#f7bf31',
+    fontWeight: '900',
+    fontSize: 33,
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  
+  },
+  titlespan: {
+    color: '#ffffff',
+    fontWeight: '200'
+  },
+  contain: {
+    flex: 1,
+     flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  row: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: -30
+  },
+  logocontainer: {
+    flexDirection: 'column',
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  btn: {
+    borderWidth: 2,
+    borderColor: '#f7bf31',
+    borderRadius: 5,
+  marginTop: 10,
+  width: "90%",
+  }
 });
+
+export default Home;
